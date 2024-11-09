c8f12657fc0f3331b24bdaba8fae3c708d88902c
chandan <chandansharma69217@gmail.com>
2024-11-09 23:17:19 +0530

    improve sorting efficiency with quickSort

--- lib/utilities/sort.dart
+++ lib/utilities/sort.dart
@@ -15,92 +15,99 @@ class Sort{
   Sort({required this.filesData}): length = filesData.length;
 
   Future<List<Data>> sortByName() async {
-    for(int i = 1; i < length; i++){
-      for(int j = 0; j< (length-i); j++){
-        var file1 = filesData[j].fileName;
-        var file2 = filesData[j+1].fileName;
-        if(file1.toLowerCase().compareTo(file2.toLowerCase())>0){
-          var temp = filesData[j];
-          filesData[j] = filesData[j+1];
-          filesData[j+1] = temp;
-        }
-      }
-    }
+    await _quickSortName(0,filesData.length - 1);
     return filesData;
   }
-
-  Future<List<Data>> sortBySize() async{
-    for(int i = 0; i<(length-1);i++){
-      for(int j = (i+1); j<length;j++){
-        var file1 = filesData[i];
-        var file2 = filesData[j];
-        if(file1.bytes >file2.bytes){
-          var temp = filesData[i];
-          filesData[i] = filesData[j];
-          filesData[j] = temp;
-        }
-      }
-    }
-    return filesData;
-  }
-
-
-
-  Future<List<Data>> sortByDate() async {
-    // Start the quicksort process on the entire list
-    await _quickSort(0, filesData.length - 1);
-    return filesData;
-  }
-
-// QuickSort helper function
-  Future<void> _quickSort(int low, int high) async {
-    if (low < high) {
-      // Partition the list and get the pivot index
-      int pivotIndex = await _partition(low, high);
-
-      // Recursively sort the two halves
-      await _quickSort(low, pivotIndex - 1); // Left sublist
-      await _quickSort(pivotIndex + 1, high); // Right sublist
+  Future<void> _quickSortName(int low,int high)async{
+    if(low<high){
+      int pivotIndex = await _partitionByName(low, high);
+      await _quickSortName(low, pivotIndex-1);
+      await _quickSortName(pivotIndex+1, high);
     }
   }
-
-
-  Future<int> _partition(int low, int high) async {
-    var pivot = _format.parse(filesData[high].date);
+  Future<int> _partitionByName(int low,int high) async {
+    var pivot = filesData[high].fileName;
     int i = low - 1;
-
-    for (int j = low; j < high; j++) {
-      var currentDate = _format.parse(filesData[j].date);
-
-      if (currentDate.isBefore(pivot)) {
+    for(int j = low;j<high;j++){
+      var fileName = filesData[j].fileName;
+      if(pivot.toLowerCase().compareTo(fileName.toLowerCase())>0){
         i++;
         var temp = filesData[i];
         filesData[i] = filesData[j];
         filesData[j] = temp;
       }
     }
+    i++;
+    var temp = filesData[i];
+    filesData[i] = filesData[high];
+    filesData[high] = temp;
+    return i;
+  }
 
+  Future<List<Data>> sortBySize() async{
+    await _quickSortBySize(0, filesData.length - 1);
+    return filesData;
+  }
+  Future<void> _quickSortBySize(int low,int high)async{
+    if(low<high){
+      int pivotIndex = await _partitionBySize(low, high);
+      await _quickSortBySize(low, pivotIndex-1);
+      await _quickSortBySize(pivotIndex+1, high);
+    }
+  }
+  Future<int> _partitionBySize(int low,int high) async{
+    var pivot = filesData[high].bytes;
+    int i = low - 1;
+    for(int j = low;j<high;j++){
+      var bytes = filesData[j].bytes;
+      if(pivot>bytes){
+        i++;
+        var temp = filesData[i];
+        filesData[i] = filesData[j];
+        filesData[j] = temp;
+      }
+    }
+    i++;
+    var temp = filesData[i];
+    filesData[i] = filesData[high];
+    filesData[high] = temp;
+    return i;
+  }
+
+
+
+  Future<List<Data>> sortByDate() async {
+    await _quickSortByDate(0, filesData.length - 1);
+    return filesData;
+  }
+  Future<void> _quickSortByDate(int low, int high) async {
+    if (low < high) {
+      int pivotIndex = await _partitionByDate(low, high);
+
+      await _quickSortByDate(low, pivotIndex - 1);
+      await _quickSortByDate(pivotIndex + 1, high);
+    }
+  }
+  Future<int> _partitionByDate(int low, int high) async {
+    var pivot = _format.parse(filesData[high].date);
+    int i = low - 1;
+    for (int j = low; j < high; j++) {
+      var currentDate = _format.parse(filesData[j].date);
+
+      if (pivot.isBefore(currentDate)) {
+        i++;
+        var temp = filesData[i];
+        filesData[i] = filesData[j];
+        filesData[j] = temp;
+      }
+    }
     var temp = filesData[i + 1];
     filesData[i + 1] = filesData[high];
     filesData[high] = temp;
-
     return i + 1;
   }
 
 
-// Future<List<Data>> sortByDate()async{
-  //   for(int i = 0; i<(length-1);i++){
-  //     for(int j = (i+1); j<length;j++){
-  //       var file1 = _format.parse(filesData[i].date);
-  //       var file2 = _format.parse(filesData[j].date);
-  //       if(file1.isBefore(file2)){
-  //         var temp = filesData[i];
-  //         filesData[i] = filesData[j];
-  //         filesData[j] = temp;
-  //       }
-  //     }
-  //   }
-  //   return filesData;
-  // }
+
 
 }
