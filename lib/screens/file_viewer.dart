df39d1f63757dc3e671639fc1ca9a5a265d23931
chandan <chandansharma69217@gmail.com>
2025-04-22 15:43:08 +0530

    fix some bugs

--- lib/screens/file_viewer.dart
+++ lib/screens/file_viewer.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:webview_flutter/webview_flutter.dart';
+
 
 // ignore_for_file: must_be_immutable
 class FileViewer extends StatefulWidget{
@@ -25,39 +25,4 @@ class _FileViewerState extends State<FileViewer> {
     );
   }
 
-}
-
-
-
-
-class OfficeFileViewer extends StatefulWidget {
-  final String fileUrl; // URL of the .docx, .pptx, .xlsx file
-
-  const OfficeFileViewer({super.key, required this.fileUrl});
-
-  @override
-  State<OfficeFileViewer> createState() => _OfficeFileViewerState();
-}
-
-class _OfficeFileViewerState extends State<OfficeFileViewer> {
-  late final WebViewController _controller;
-
-  @override
-  void initState() {
-    super.initState();
-    _controller = WebViewController()
-      ..loadRequest(
-        Uri.parse(
-          "https://view.officeapps.live.com/op/embed.aspx?src=${Uri.encodeComponent(widget.fileUrl)}",
-        ),
-      );
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text("Office File Viewer")),
-      body: WebViewWidget(controller: _controller),
-    );
-  }
 }
