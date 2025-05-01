ba519a118b69f29a24ab083a734fafb770807c60
chandan <chandansharma69217@gmail.com>
2025-05-01 12:42:46 +0530

    fix some bugs

--- lib/screens/navigation_screens/bottom/tools_screen.dart
+++ lib/screens/navigation_screens/bottom/tools_screen.dart
@@ -13,8 +13,6 @@ import 'package:pdf_reader/all_tools/edit_tools/split_PDF.dart';
 import 'package:pdf_reader/all_tools/manage_tools/lock_pdf.dart';
 import 'package:pdf_reader/all_tools/manage_tools/unlock_pdf.dart';
 import 'package:pdf_reader/all_tools/manage_tools/watermark_pdf.dart';
-import 'package:pdf_reader/api/stirling_pdf.dart';
-import 'package:pdf_reader/pdf_sign_screen.dart';
 import 'package:pdf_reader/screens/selection_screen.dart';
 import 'package:pdf_reader/utilities/color_theme.dart';
 
